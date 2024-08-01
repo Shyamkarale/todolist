31786686418c44f35ae30e47cd3c49f9394a9173
Shyam Karale <shyamkarale168@gmail.com>
2024-08-01 22:54:13 +0530

    Duplication of data is prevented in new version

--- 2do.js
+++ 2do.js
@@ -1,37 +1,56 @@
 const inputbox = document.getElementById("input-box");
 const listcontainer = document.getElementById("list-container");
 
+function addtask() {
+    if (inputbox.value === '') {
+        alert("You must write something");
+    } else {
+        // Check for duplicates
+        const items = listcontainer.getElementsByTagName('li');
+        let isDuplicate = false;
+        for (let item of items) {
+            if (item.firstChild.textContent === inputbox.value.trim()) {
+                isDuplicate = true;
+                break;
+            }
+        }
 
-function addtask(){
-    if(inputbox.value ===''){
-        alert("You must Write Something");
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputbox.value;
-        listcontainer.appendChild(li);
-        let span = document.createElement("span")
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+        if (isDuplicate) {
+            alert("This task already exists!");
+        } else {
+            // Create new list item
+            let li = document.createElement("li");
+            li.textContent = inputbox.value.trim();  // Using textContent instead of innerHTML
+            listcontainer.appendChild(li);
+
+            // Create delete button
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
+        }
     }
     inputbox.value = "";
     savedata();
 }
 
-listcontainer.addEventListener("click",function(e){
-    console.log(e.target.tagName)
-    if(e.target.tagName === "LI"){
-    e.target.classList.toggle("checked");
+listcontainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        savedata(); // Save data after removing an item
+    }
+}, false);
+
+function savedata() {
+    localStorage.setItem("data", listcontainer.innerHTML);
 }
-else if (e.target.tagName === "SPAN"){
-    console.log(e.target.parentElement)
-    e.target.parentElement.remove();
+
+function showTask() {
+    const data = localStorage.getItem("data");
+    if (data) {
+        listcontainer.innerHTML = data;
+    }
 }
-},false);
-function savedata(){
-    localStorage.setItem("data",listcontainer.innerHTML);
-}
-function showTask(){
-    listcontainer.innerHTML = localStorage.getItem("data");
-}
+
 showTask();
